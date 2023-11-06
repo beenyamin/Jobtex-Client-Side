4baf2285fbc5b97b5a81d189a8db16ab62b62e3c
gsmkawsar <support@bestflashfile.com>
2023-11-06 15:38:00 +0600

    AuthProvider completed

--- src/Hook/AuthProvider.jsx
+++ src/Hook/AuthProvider.jsx
@@ -1,10 +1,23 @@
+import { createContext, useState } from "react";
+import app from "./firebase.config";
+import { getAuth } from "firebase/auth";
 
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
-const AuthProvider = () => {
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const AuthInfo = {
+
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={AuthInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from './Router/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './Hook/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
       <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
